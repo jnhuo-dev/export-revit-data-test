b475bb2ffa0f9c7118d1bf11b566b37591e58914
Park Junho <jnhuo@naver.com>
2025-08-02 16:26:51 +0900

    importCommand 수정

--- exportDB/ImportCommand.cs
+++ exportDB/ImportCommand.cs
@@ -1,11 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
 using Autodesk.Revit.Attributes;
 using Autodesk.Revit.DB;
 using Autodesk.Revit.UI;
+using Newtonsoft.Json;
 
 namespace exportDB
 {
@@ -14,40 +16,70 @@ namespace exportDB
     {
         public Result Execute(ExternalCommandData commandData, ref string message, ElementSet elements)
         {
-            //Get the Current Session / Project from Revit
             UIApplication uiApp = commandData.Application;
+            UIDocument uidoc = uiApp.ActiveUIDocument;
+            Document doc = uidoc.Document;
 
-            //Get the Current Document from the Current Session
-            Document doc = uiApp.ActiveUIDocument.Document;
+            var task = Task.Run(() => GetDoorsFromServer());
+            task.Wait(); // async → sync
+            var doorDataList = task.Result;
 
-            //REST request to GET door data 
-            List<Door> doors = DoorAPI.Get("doors");
+            if (doorDataList == null)
+                return Result.Failed;
 
-            //Set Door finish values using unique
-            if (doors != null && doors.Count > 0)
+            using (Transaction tx = new Transaction(doc, "문 정보 업데이트"))
             {
-                using (Transaction trans = new Transaction(doc, "Import Door Data"))
+                tx.Start();
+
+                foreach (Element door in new FilteredElementCollector(doc).OfCategory(BuiltInCategory.OST_Doors).WhereElementIsNotElementType())
                 {
-                    trans.Start();
-
-                    foreach (Door door in doors)
+                    var match = doorDataList.FirstOrDefault(d => d._id == door.UniqueId);
+                    if (match != null)
                     {
-                        string uId = door._id;
-                        Element element = doc.GetElement(uId);
-
-                        string dFinish = door.DoorFinish;
-                        Parameter doorFinish = element.get_Parameter(BuiltInParameter.DOOR_FINISH);
-
-                        doorFinish.Set(dFinish);
+                        door.LookupParameter("Mark")?.Set(match.Mark ?? "");
+                        door.LookupParameter("DoorFinish")?.Set(match.DoorFinish ?? "");
                     }
-
-                    trans.Commit();
                 }
+
+                tx.Commit();
             }
 
-            TaskDialog.Show("Import Door Data", "Door Data successfuly imported");
-
+            TaskDialog.Show("완료", "서버에서 문 정보를 불러와 업데이트했습니다.");
             return Result.Succeeded;
+        }
+
+        private async Task<List<DoorInfo>> GetDoorsFromServer()
+        {
+            using (HttpClient client = new HttpClient())
+            {
+                try
+                {
+                    var response = await client.GetAsync("http://localhost:4000/doors");
+
+                    if (!response.IsSuccessStatusCode)
+                    {
+                        TaskDialog.Show("오류", "서버 응답 실패: " + response.StatusCode);
+                        return null;
+                    }
+
+                    string json = await response.Content.ReadAsStringAsync();
+                    var doorList = JsonConvert.DeserializeObject<List<DoorInfo>>(json);
+                    return doorList;
+                }
+                catch (Exception ex)
+                {
+                    TaskDialog.Show("예외", ex.Message);
+                    return null;
+                }
+            }
+        }
+
+        public class DoorInfo
+        {
+            public string _id { get; set; } // UniqueId
+            public string FamilyType { get; set; }
+            public string Mark { get; set; }
+            public string DoorFinish { get; set; }
         }
     }
 }
